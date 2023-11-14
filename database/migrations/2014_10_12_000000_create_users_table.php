89c2f0922795aa52d4358daa27ad67d0a49f1de7
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-11-14 15:44:19 +0200

    Server <> created users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -10,12 +10,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('bio')->nullable();
+            $table->string('phone');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('password')->min(8);
+            $table->string('profile_picture')->nullable();
         });
     }
     
